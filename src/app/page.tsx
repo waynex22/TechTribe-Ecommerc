558e68b27a7066dca34a017db2a354b765493e15
waynex22 <acquyxn@gmail.com>
2024-06-07 19:49:18 +0700

    change-f

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-red-500">Wellcome to TechTribe Ecommerc</div>
-    </main>
-  );
-}
